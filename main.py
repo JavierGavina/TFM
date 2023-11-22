33612d24d3089fd6b010db2dd8c495831f35c62e
JavierGavina <gavina@uji.es>
2023-11-22 10:31:52 +0100

    final positioning estimation for data augmentation and synthetized data and reduced number of cores in multiprocessing

--- main.py
+++ main.py
@@ -30,11 +30,12 @@ if __name__ == "__main__":
     print("OBTENIENDO DATOS CON RADIOMAP AUMENTADO CON MALLADO CONTINUO")
     print("===============================")
 
-    get_loader = DataLoader(data_dir=f"../{constants.data.train.FINAL_PATH}/groundtruth.csv",
+    get_loader = DataLoader(data_dir=f"{constants.data.train.FINAL_PATH}/groundtruth.csv",
                             aps_list=constants.aps, batch_size=30, step_size=5,
                             size_reference_point_map=28, return_axis_coords=True)
 
-    rpmap, _, [x_coords, y_coords] = get_loader()
+    X, _, [x_coords, y_coords] = get_loader()
+    rpmap = X[:, :, :, 0]
     radiomap_extended = get_radiomap_from_rpmap(rpmap, x_coords, y_coords)
     Xtrain_RPMAP, ytrain_RPMAP = radiomap_extended[constants.aps].to_numpy(), radiomap_extended[
         ["Longitude", "Latitude"]].to_numpy()
@@ -43,14 +44,14 @@ if __name__ == "__main__":
     print("OBTENIENDO DATOS AUMENTADOS CON GAN")
     print("===============================")
 
-    data_augmentation = DataAugmentation(
-        path_to_generator=f"{constants.outputs.models.cgan_28}/checkpoints/c_gan499.h5")
+    data_augmentation = DataAugmentation(path_to_generator=f"{constants.outputs.models.cgan_28}/checkpoints/c_gan499.h5")
     generated, input_labels = data_augmentation(n_samples_per_label=30)
+
     samples_per_ap = int(rpmap.shape[0] / len(constants.aps))
     samples_generated_per_ap = int(generated.shape[0] / len(constants.aps))
-    rpmap_ext = np.zeros(
-        (rpmap.shape[0] + samples_generated_per_ap * len(constants.aps), rpmap.shape[1], rpmap.shape[2]))
+    rpmap_ext = np.zeros((rpmap.shape[0] + samples_generated_per_ap * len(constants.aps), rpmap.shape[1], rpmap.shape[2]))
     count_gen = 0
+
     for n_ap, ap in enumerate(constants.aps):
         for batch_temporal in range(samples_per_ap):
             id_row = n_ap * samples_per_ap + batch_temporal
@@ -61,17 +62,15 @@ if __name__ == "__main__":
             count_gen += 1
 
     radiomap_generated = get_radiomap_from_rpmap(rpmap_ext, x_coords, y_coords)
-    Xtrain_generated, ytrain_generated = radiomap_generated[constants.aps].to_numpy(), radiomap_generated[
-        ["Longitude", "Latitude"]].to_numpy()
+    Xtrain_generated, ytrain_generated = radiomap_generated[constants.aps].to_numpy(), radiomap_generated[["Longitude", "Latitude"]].to_numpy()
 
     print("===============================")
     print("OBTENIENDO DATOS SINTÉTICOS CON GAN")
     print("===============================")
 
-    synthetic = data_augmentation(n_samples_per_label=300)
-    radiomap_synthetic, _ = get_radiomap_from_rpmap(synthetic, x_coords, y_coords)
-    Xtrain_synthetic, ytrain_synthetic = radiomap_synthetic[constants.aps].to_numpy(), radiomap_synthetic[
-        ["Longitude", "Latitude"]].to_numpy()
+    synthetic, _ = data_augmentation(n_samples_per_label=300)
+    radiomap_synthetic = get_radiomap_from_rpmap(synthetic, x_coords, y_coords)
+    Xtrain_synthetic, ytrain_synthetic = radiomap_synthetic[constants.aps].to_numpy(), radiomap_synthetic[["Longitude", "Latitude"]].to_numpy()
 
     print("===============================")
     print("OBTENIENDO DATOS DE TEST")
@@ -89,13 +88,13 @@ if __name__ == "__main__":
     print("ENTRENANDO CON LOS DATOS ORIGINALES")
     print("===============================")
 
-    knn = GridSearchCV(KNeighborsRegressor(n_jobs=multiprocessing.cpu_count() - 1),
+    knn = GridSearchCV(KNeighborsRegressor(n_jobs=multiprocessing.cpu_count() - 3),
                        param_grid={"n_neighbors": param_neighbors},
-                       cv=5, n_jobs=multiprocessing.cpu_count() - 1).fit(Xtrain, ytrain)
+                       cv=5, n_jobs=multiprocessing.cpu_count() - 3).fit(Xtrain, ytrain)
 
-    rf = GridSearchCV(RandomForestRegressor(n_jobs=multiprocessing.cpu_count() - 1),
+    rf = GridSearchCV(RandomForestRegressor(n_jobs=multiprocessing.cpu_count() - 3),
                       param_grid={"n_estimators": param_n_estimators},
-                      cv=5, n_jobs=multiprocessing.cpu_count() - 1).fit(Xtrain, ytrain)
+                      cv=5, n_jobs=multiprocessing.cpu_count() - 3).fit(Xtrain, ytrain)
 
     print("best params: ")
     print(">>> knn: ", knn.best_params_)
@@ -105,13 +104,13 @@ if __name__ == "__main__":
     print("ENTRENANDO CON LOS DATOS DE MALLADO CONTINUO")
     print("===============================")
 
-    knn_RPMAP = GridSearchCV(KNeighborsRegressor(n_jobs=multiprocessing.cpu_count() - 1),
+    knn_RPMAP = GridSearchCV(KNeighborsRegressor(n_jobs=multiprocessing.cpu_count() - 3),
                              param_grid={"n_neighbors": param_neighbors},
-                             cv=5, n_jobs=multiprocessing.cpu_count() - 1).fit(Xtrain_RPMAP, ytrain_RPMAP)
+                             cv=5, n_jobs=multiprocessing.cpu_count() - 3).fit(Xtrain_RPMAP, ytrain_RPMAP)
 
-    rf_RPMAP = GridSearchCV(RandomForestRegressor(n_jobs=multiprocessing.cpu_count() - 1),
+    rf_RPMAP = GridSearchCV(RandomForestRegressor(n_jobs=multiprocessing.cpu_count() - 3),
                             param_grid={"n_estimators": param_n_estimators},
-                            cv=5, n_jobs=multiprocessing.cpu_count() - 1).fit(Xtrain_RPMAP, ytrain_RPMAP)
+                            cv=5, n_jobs=multiprocessing.cpu_count() - 3).fit(Xtrain_RPMAP, ytrain_RPMAP)
 
     print("best params: ")
     print(">>> knn_RPMAP: ", knn_RPMAP.best_params_)
@@ -121,13 +120,13 @@ if __name__ == "__main__":
     print("ENTRENANDO CON LOS DATOS AUMENTADOS CON GAN")
     print("===============================")
 
-    knn_generated = GridSearchCV(KNeighborsRegressor(n_jobs=multiprocessing.cpu_count() - 1),
+    knn_generated = GridSearchCV(KNeighborsRegressor(n_jobs=multiprocessing.cpu_count() - 3),
                                  param_grid={"n_neighbors": param_neighbors},
-                                 cv=5, n_jobs=multiprocessing.cpu_count() - 1).fit(Xtrain_generated, ytrain_generated)
+                                 cv=5, n_jobs=multiprocessing.cpu_count() - 3).fit(Xtrain_generated, ytrain_generated)
 
-    rf_generated = GridSearchCV(RandomForestRegressor(n_jobs=multiprocessing.cpu_count() - 1),
+    rf_generated = GridSearchCV(RandomForestRegressor(n_jobs=multiprocessing.cpu_count() - 3),
                                 param_grid={"n_estimators": param_n_estimators},
-                                cv=5, n_jobs=multiprocessing.cpu_count() - 1).fit(Xtrain_generated, ytrain_generated)
+                                cv=5, n_jobs=multiprocessing.cpu_count() - 3).fit(Xtrain_generated, ytrain_generated)
 
     print("best params: ")
     print(">>> knn_generated: ", knn_generated.best_params_)
@@ -137,13 +136,13 @@ if __name__ == "__main__":
     print("ENTRENANDO CON LOS DATOS SINTÉTICOS CON GAN")
     print("===============================")
 
-    knn_synthetic = GridSearchCV(KNeighborsRegressor(n_jobs=multiprocessing.cpu_count() - 1),
+    knn_synthetic = GridSearchCV(KNeighborsRegressor(n_jobs=multiprocessing.cpu_count() - 3),
                                  param_grid={"n_neighbors": param_neighbors},
-                                 cv=5, n_jobs=multiprocessing.cpu_count() - 1).fit(Xtrain_synthetic, ytrain_synthetic)
+                                 cv=5, n_jobs=multiprocessing.cpu_count() - 3).fit(Xtrain_synthetic, ytrain_synthetic)
 
-    rf_synthetic = GridSearchCV(RandomForestRegressor(n_jobs=multiprocessing.cpu_count() - 1),
+    rf_synthetic = GridSearchCV(RandomForestRegressor(n_jobs=multiprocessing.cpu_count() - 3),
                                 param_grid={"n_estimators": param_n_estimators},
-                                cv=5, n_jobs=multiprocessing.cpu_count() - 1).fit(Xtrain_synthetic, ytrain_synthetic)
+                                cv=5, n_jobs=multiprocessing.cpu_count() - 3).fit(Xtrain_synthetic, ytrain_synthetic)
 
     print("best params: ")
     print(">>> knn_synthetic: ", knn_synthetic.best_params_)
@@ -203,12 +202,17 @@ if __name__ == "__main__":
 
     print(f">> KNN --- mean: {np.mean(sorted_knn):.2f}, 75% perc: {np.percentile(sorted_knn, 75):.2f}")
     print(f">> RF --- mean: {np.mean(sorted_rf):.2f}, 75% perc: {np.percentile(sorted_rf, 75):.2f}")
-    print(f">> KNN RPMAP --- mean: {np.mean(sorted_knn_RPMAP):.2f}, 75% perc: {np.percentile(sorted_knn_RPMAP, 75):.2f}")
+    print(
+        f">> KNN RPMAP --- mean: {np.mean(sorted_knn_RPMAP):.2f}, 75% perc: {np.percentile(sorted_knn_RPMAP, 75):.2f}")
     print(f">> RF RPMAP --- mean: {np.mean(sorted_rf_RPMAP):.2f}, 75% perc: {np.percentile(sorted_rf_RPMAP, 75):.2f}")
-    print(f">> KNN GAN --- mean: {np.mean(sorted_knn_generated):.2f}, 75% perc: {np.percentile(sorted_knn_generated, 75):.2f}")
-    print(f">> RF GAN --- mean: {np.mean(sorted_rf_generated):.2f}, 75% perc: {np.percentile(sorted_rf_generated, 75):.2f}")
-    print(f">> KNN SYNTHETIC --- mean: {np.mean(sorted_knn_synthetic):.2f}, 75% perc: {np.percentile(sorted_knn_synthetic, 75):.2f}")
-    print(f">> RF SYNTHETIC --- mean: {np.mean(sorted_rf_synthetic):.2f}, 75% perc: {np.percentile(sorted_rf_synthetic, 75):.2f}")
+    print(
+        f">> KNN GAN --- mean: {np.mean(sorted_knn_generated):.2f}, 75% perc: {np.percentile(sorted_knn_generated, 75):.2f}")
+    print(
+        f">> RF GAN --- mean: {np.mean(sorted_rf_generated):.2f}, 75% perc: {np.percentile(sorted_rf_generated, 75):.2f}")
+    print(
+        f">> KNN SYNTHETIC --- mean: {np.mean(sorted_knn_synthetic):.2f}, 75% perc: {np.percentile(sorted_knn_synthetic, 75):.2f}")
+    print(
+        f">> RF SYNTHETIC --- mean: {np.mean(sorted_rf_synthetic):.2f}, 75% perc: {np.percentile(sorted_rf_synthetic, 75):.2f}")
 
     results = pd.DataFrame(columns=["method", "mean_error_distance", "percentile_75%_error"])
     results.loc[0] = [f"knn (k={knn.best_params_['n_neighbors']})", np.mean(sorted_knn), np.percentile(sorted_knn, 75)]
